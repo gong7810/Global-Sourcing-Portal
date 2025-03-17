ddead269e0831f435ac8792e4a8a98e64633c605
yjc0901 <yayaz96@naver.com>
2025-03-17 11:54:40 +0900

    구직자 이용약관 수정

---
+++ src/components/terms/OptionalPrivacyTerms.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="optional-privacy-terms">
+      <strong>1. 수집 이용 목적</strong>
+    <p class="mb-4 ml-4">
+      <u>서비스 안내 목적으로 활용</u>
+    </p>
+
+    <strong>2. 수집하는 개인정보 항목</strong>
+    <p class="mb-4 ml-4">
+      전화번호, 이메일, 서비스 이용 기록
+    </p>
+
+    <strong>3. 개인정보 보유 및 이용기간</strong>
+    <p class="mb-4 ml-4">
+      <u>회원탈퇴 시 즉시 파기</u>
+    </p>
+
+    <strong>4. 수신동의 거부 및 철회방법 안내</strong>
+    <p class="mb-4 ml-4">
+      본 동의는 거부하실 수 있습니다. 다만 거부 시 동의를 통해 제공 가능한 각종 혜택, 이벤트 안내를 받아보실 수 없습니다.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.optional-privacy-terms {
+  font-size: 14px;
+  line-height: 1.6;
+}
+
+p {
+  color: #666;
+}
+</style> 
