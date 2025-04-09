d195937934a9a4178d16b08117a5a1fb721c25c6
gong7810 <gong5518@naver.com>
2025-04-10 02:15:14 +0900

    ✨ utc기준 날짜 포맷팅 복원 유틸 함수 구현

--- src/plugins/commonutils.js
+++ src/plugins/commonutils.js
@@ -52,23 +52,52 @@ export const useDynamicDialog = () => {
   };
 };
 
-// export const useConvertCode = () => {
-//   const convert = async (codeList, code) => {
-//     if (!code) return '';
-
-//     let name = '';
-
-//     codeList.map((item) => {
-//       if (item.code === code) {
-//         name = item.name;
-//         return;
-//       }
-//     });
-
-//     return name;
-//   };
-
-//   return {
-//     convert
-//   };
-// };
+export const useDateFormatter = () => {
+  return {
+    fullDate: (dateString) => {
+      if (!dateString) return '';
+      const date = new Date(dateString);
+      return date.toLocaleString('ko-KR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true, // 오전/오후 표시
+        timeZone: 'Asia/Seoul' // UTC 시간대로 설정
+      });
+    },
+    halfDate: (dateString) => {
+      if (!dateString) return '';
+      const date = new Date(dateString);
+      return date
+        .toLocaleString('ko-KR', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true, // 오전/오후 표시
+          timeZone: 'Asia/Seoul' // UTC 시간대로 설정
+        })
+        .slice(0, 12)
+        .replaceAll(' ', '');
+    },
+    quartDate: (dateString) => {
+      if (!dateString) return '';
+      const date = new Date(dateString);
+      return date
+        .toLocaleString('ko-KR', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true, // 오전/오후 표시
+          timeZone: 'Asia/Seoul' // UTC 시간대로 설정
+        })
+        .slice(0, 8)
+        .replaceAll(' ', '');
+    }
+  };
+};
